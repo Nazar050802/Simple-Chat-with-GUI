5b81f4453df143ca03a007d1ad5ae2940e12a33d
Nazar050802 <nazar050802@gmail.com>
2023-06-23 15:15:58 +0200

    Add class Core and Add+Implement class IpAddress

---
+++ Backend/ServerSide/ServerSide/ServerSide/IpAddress.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSide
+{
+    public interface IpAdressProvider
+    {
+        IPAddress GetIPAddress();
+    }
+
+    public class LocalHostIpAdressProvider : IpAdressProvider
+    {
+        public IPAddress GetIPAddress()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            return ipHost.AddressList[0];
+        }
+    }
+
+    public class ExternalIpAdressProvider : IpAdressProvider
+    {
+        protected string IpAddress { get; set; }
+
+        public ExternalIpAdressProvider(string ipAddress = "127.0.0.1")
+        {
+            IpAddress = ipAddress;
+        }
+
+        public IPAddress GetIPAddress() 
+        { 
+            return IPAddress.Parse(IpAddress);
+        }
+    }
+
+    internal class IpAddress
+    {
+        private bool DebugMode { get; set; }
+        private IpAdressProvider IpAddressString { get; set;}
+
+        private int Port { get; set; }
+
+        public IpAddress(bool debugMode, string ipAddressString = "127.0.0.1", int port = 11000)
+        {
+            DebugMode = debugMode;
+            Port = port;
+
+            IpAddressString = debugMode ? (IpAdressProvider) new LocalHostIpAdressProvider() : new ExternalIpAdressProvider(ipAddressString);
+        }
+
+        public IPEndPoint GetIPEndPoint()
+        {
+            return new IPEndPoint(IpAddressString.GetIPAddress(), Port);
+        }
+    }
+}
